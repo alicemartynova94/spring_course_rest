291868d630f5bd8773436f6d2f9da7d355ee03e4
alicemartynova94 <alicemartynova94@gmail.ru>
2024-03-04 15:25:30 +0300

    i

---
+++ src/main/java/com/alisia/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,54 @@
+package com.alisia.spring.rest.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.alisia.spring.rest")
+@EnableWebMvc
+@EnableTransactionManagement
+public class MyConfig {
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        try {
+            comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test?useSSL=false&amp;serverTimezone=UTC");
+            comboPooledDataSource.setUser("root");
+            comboPooledDataSource.setPassword("");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return comboPooledDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.alisia.spring.rest.entity");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(localSessionFactoryBean().getObject());
+        return transactionManager;
+    }
+}
