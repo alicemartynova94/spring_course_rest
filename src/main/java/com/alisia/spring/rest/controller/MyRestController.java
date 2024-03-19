5e9e9c453444c1cde77955a5f96a761dbbc18b82
alicemartynova94 <alicemartynova94@gmail.ru>
2024-03-19 19:25:46 +0300

    add get method to display all employees

--- src/main/java/com/alisia/spring/rest/controller/MyRestController.java
+++ src/main/java/com/alisia/spring/rest/controller/MyRestController.java
@@ -1,8 +1,26 @@
 package com.alisia.spring.rest.controller;
 
+import com.alisia.spring.rest.entity.Employee;
+import com.alisia.spring.rest.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class MyRestController {
+    private final EmployeeService employeeService;
+
+    public MyRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
 
 }
