e6969b6ee8feb1f7ec29da940ce0ca039aa9656a
alicemartynova94 <alicemartynova94@gmail.ru>
2024-03-20 15:11:01 +0300

    add exception handler package with classes to handle incorrect employee id

---
+++ src/main/java/com/alisia/spring/rest/exception_handler/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package com.alisia.spring.rest.exception_handler;
+
+public class NoSuchEmployeeException extends RuntimeException{
+
+    public NoSuchEmployeeException(String message){
+        super(message);
+    }
+}
